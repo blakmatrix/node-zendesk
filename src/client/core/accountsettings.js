8a303987d8928da2432c0e864663810fa6725437
Farrin Reid <blakmatrix@gmail.com>
2023-08-28 18:27:12 -0700

    added JSDoc comments to accountsettings + added `update` method

--- src/client/core/accountsettings.js
+++ src/client/core/accountsettings.js
@@ -2,15 +2,40 @@
 
 const {Client} = require('../client');
 
+/**
+ * The AccountSettings class provides methods for interacting with account settings in the Zendesk JSON API.
+ * {@link https://developer.zendesk.com/api-reference/ticketing/account-configuration/account_settings/ | See the Zendesk API documentation for more details}.
+ * @extends Client
+ */
 class AccountSettings extends Client {
   constructor(options) {
     super(options);
     this.jsonAPINames = ['settings'];
   }
 
-  // Listing AccountSettings
+  /**
+   * Retrieves the account settings.
+   * @async
+   * @returns {Promise<Object>} A promise that resolves to the account settings.
+   * {@link https://developer.zendesk.com/api-reference/ticketing/account-configuration/account_settings/#show-settings | See the Zendesk API documentation for more details}.
+   * @example
+   * const settings = await zd.accountSettings.show();
+   */
   async show() {
     return this.get(['account', 'settings']);
+  }
+
+  /**
+   * Updates the account settings.
+   * @async
+   * @param {Object} settings - The settings to update.
+   * @returns {Promise<Object>} A promise that resolves to the updated account settings.
+   * {@link https://developer.zendesk.com/api-reference/ticketing/account-configuration/account_settings/#update-account-settings | See the Zendesk API documentation for more details}.
+   * @example
+   * const settings = await zd.accountSettings.update({ "settings": { "active_features": { "customer_satisfaction": false }}});
+   */
+  async update(settings) {
+    return this.put(['account', 'settings'], settings);
   }
 }
 
