a42510b109e1f421bf9b5921f39d209aae0eb291
Farrin Reid <blakmatrix@gmail.com>
2023-09-08 14:34:28 -0700

    feat: Enhance OrganizationFields with JSDoc and new reorder method
    - Added detailed JSDoc comments with @see links pointing to Zendesk API documentation for each method.
    - Introduced a new `reorder` method for OrganizationFields.
    - Improved clarity and consistency of the example usages in the JSDoc comments.

--- src/client/core/organizationfields.js
+++ src/client/core/organizationfields.js
@@ -1,28 +1,61 @@
 // Organizationfields.js: Client for the zendesk API.
 const {Client} = require('../client');
 
+/**
+ * Class representing the OrganizationFields API endpoints.
+ * @see {@link https://developer.zendesk.com/api-reference/ticketing/organizations/organization_fields/}
+ */
 class OrganizationFields extends Client {
   constructor(options) {
     super(options);
     this.jsonAPINames = ['organization_fields', 'organization_field'];
   }
 
-  // Listing OrganizationFields
+  /**
+   * List all organization fields.
+   * @async
+   * @see {@link https://developer.zendesk.com/api-reference/ticketing/organizations/organization_fields/#list-organization-fields}
+   * @returns {Promise<Object>} The organization fields.
+   * @example const orgFields = await client.organizationfields.list();
+   */
   async list() {
     return this.getAll(['organization_fields']);
   }
 
-  // Viewing OrganizationFields
+  /**
+   * Show a specific organization field.
+   * @async
+   * @param {number} organizationFieldID - The ID of the organization field.
+   * @returns {Promise<Object>} The organization field details.
+   * @throws {Error} Throws an error if the organization field is not found.
+   * @see {@link https://developer.zendesk.com/api-reference/ticketing/organizations/organization_fields/#show-organization-field}
+   * @example const orgField = await client.organizationfields.show(12345);
+   */
   async show(organizationFieldID) {
     return this.get(['organization_fields', organizationFieldID]);
   }
 
-  // Creating OrganizationFields
+  /**
+   * Create a new organization field.
+   * @async
+   * @param {Object} organizationField - The organization field object.
+   * @returns {Promise<Object>} The newly created organization field.
+   * @see {@link https://developer.zendesk.com/api-reference/ticketing/organizations/organization_fields/#create-organization-field}
+   * @example const newOrgField = await client.organizationfields.create({ type: 'text', title: 'Support description' });
+   */
   async create(organizationField) {
     return this.post(['organization_fields'], organizationField);
   }
 
-  // Updating OrganizationFields
+  /**
+   * Update an existing organization field.
+   * @async
+   * @param {number} organizationFieldID - The ID of the organization field to update.
+   * @param {Object} organizationField - The updated organization field object.
+   * @returns {Promise<Object>} The updated organization field.
+   * @see {@link https://developer.zendesk.com/api-reference/ticketing/organizations/organization_fields/#update-organization-field}
+   * @example const updatedOrgField = await client.organizationfields.update(12345, { title: 'New title' });
+   */
   async update(organizationFieldID, organizationField) {
     return this.put(
       ['organization_fields', organizationFieldID],
@@ -30,9 +63,31 @@ class OrganizationFields extends Client {
     );
   }
 
-  // Deleting OrganizationFields
+  /**
+   * Delete an organization field.
+   * @async
+   * @param {number} organizationFieldID - The ID of the organization field to delete.
+   * @returns {Promise<Object>} Response indicating the deletion status.
+   * @throws {Error} Throws an error if the deletion fails.
+   * @see {@link https://developer.zendesk.com/api-reference/ticketing/organizations/organization_fields/#delete-organization-field}
+   * @example await client.organizationfields.delete(12345);
+   */
   async delete(organizationFieldID) {
     return super.delete(['organization_fields', organizationFieldID]);
+  }
+
+  /**
+   * Reorder the organization fields.
+   * @async
+   * @param {Array<number>} organizationFieldIds - An array of organization field IDs in the desired order.
+   * @returns {Promise<Object>} The reordered organization fields.
+   * @see {@link https://developer.zendesk.com/api-reference/ticketing/organizations/organization_fields/#reorder-organization-field}
+   * @example await client.organizationfields.reorder([3, 4]);
+   */
+  async reorder(organizationFieldIds) {
+    return this.put(['organization_fields', 'reorder'], {
+      organization_field_ids: organizationFieldIds,
+    });
   }
 }
 
