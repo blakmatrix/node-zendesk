592be6c15bdf0fb9e72b9d7913378eb8f3c074b6
Farrin Reid <blakmatrix@gmail.com>
2023-09-07 21:08:12 -0700

    commit message:
    feat(imports): add JSDoc for ticket import methods
    
    - Add detailed JSDoc comments for the Imports class and its methods.
    - Provide descriptions, parameter details, example usage, and references to Zendesk API documentation.
    - Enhance documentation clarity for better developer experience.

--- src/client/core/imports.js
+++ src/client/core/imports.js
@@ -1,21 +1,66 @@
-// Imports.js: Client for the zendesk API.
+// File: imports.js
 const {Client} = require('../client');
 
+/**
+ * Client for the Zendesk Ticket Import API.
+ * @see {@link https://developer.zendesk.com/api-reference/ticketing/tickets/ticket_import/}
+ */
 class Imports extends Client {
   constructor(options) {
     super(options);
     this.jsonAPINames = ['tickets', 'ticket'];
   }
 
-  // Import Ticket
+  /**
+   * Imports a single ticket to Zendesk.
+   * @async
+   * @param {Object} ticket - The ticket data to be imported.
+   * @returns {Promise<Object>} The response from the Zendesk API.
+   * @throws {Error} Throws an error if the request fails.
+   * @example
+   * const ticketData = {
+   *   ticket: {
+   *     assignee_id: 19,
+   *     comments: [{
+   *       author_id: 827,
+   *       created_at: "2009-06-25T10:15:18Z",
+   *       value: "This is a comment"
+   *     }],
+   *     description: "A description",
+   *     requester_id: 827,
+   *     subject: "Help"
+   *   }
+   * };
+   * const response = await client.imports.ticket(ticketData);
+   * @see {@link https://developer.zendesk.com/api-reference/ticketing/tickets/ticket_import/#ticket-import}
+   */
   async ticket(ticket) {
     return this.post(['imports', 'tickets'], ticket);
   }
 
-  // Import Many Tickets
+  /**
+   * Imports multiple tickets to Zendesk in bulk.
+   * @async
+   * @param {Object[]} tickets - An array of ticket data to be imported.
+   * @returns {Promise<Object>} The response from the Zendesk API.
+   * @throws {Error} Throws an error if the request fails.
+   * @example
+   * const ticketsData = [{
+   *   assignee_id: 19,
+   *   comments: [{
+   *     author_id: 827,
+   *     created_at: "2009-06-25T10:15:18Z",
+   *     value: "This is a comment"
+   *   }],
+   *   description: "A description",
+   *   requester_id: 827,
+   *   subject: "Help"
+   * }];
+   * const response = await client.imports.ticketMany(ticketsData);
+   * @see {@link https://developer.zendesk.com/api-reference/ticketing/tickets/ticket_import/#ticket-bulk-import}
+   */
   async ticketMany(tickets) {
     return this.requestAll(
-      // TODO: postAll
       'POST',
       ['imports', 'tickets', 'create_many'],
       tickets,
