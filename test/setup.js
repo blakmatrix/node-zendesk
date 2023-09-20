db415bdf545f33752947a16ebc27b288ca0ec5cf
Joshua Teitelbaum <jteitelbaum@zendesk.com>
2023-09-20 09:17:39 -0700

    checkpoint

--- test/setup.js
+++ test/setup.js
@@ -4,23 +4,15 @@ import process from 'node:process';
 import crypto from 'node:crypto';
 import dotenv from 'dotenv';
 import {createClient} from '../src/index.js';
+import request from "request";
 
 dotenv.config();
 
-const username = process.env.ZENDESK_USERNAME;
-const subdomain = process.env.ZENDESK_SUBDOMAIN;
-const token = process.env.ZENDESK_TOKEN;
+const {ZENDESK_USERNAME, ZENDESK_SUBDOMAIN, ZENDESK_TOKEN} = process.env;
 
-export const initializeClient = (config) => {
-  return createClient({username, subdomain, ...config});
-};
 
-export const setupClient = (config = {}) => {
-  return initializeClient({token, ...config});
-};
 
-export const generateOrganizationName = () => {
-  const id = crypto.randomBytes(16).toString('hex');
+export const generateOrganizationName = (id) => {
   return `Test Organization ${id}`;
 };
 
@@ -28,9 +20,63 @@ export const generateMultipleOrganizations = (n) => {
   const organizations = [];
   for (let i = 0; i < n; i++) {
     organizations.push({
-      name: generateOrganizationName(),
+      name: generateOrganizationName(i),
     });
   }
 
   return organizations;
 };
+export const transportConfigUsingRequest = {
+  transportFn(uri, options) {
+    // Convert the options to be compatible with the request library
+    const requestOptions = {
+      ...options,
+      uri,
+      headers: options.headers,
+    };
+
+    return new Promise((resolve, reject) => {
+      request(requestOptions, (error, response, body) => {
+        if (error) {
+          reject(error);
+        } else {
+          // Include the body in the response object for the adapter to handle
+          response.body = body;
+          resolve(response);
+        }
+      });
+    });
+  },
+
+  responseAdapter(response) {
+    return {
+      json() {
+        try {
+          return Promise.resolve(JSON.parse(response.body));
+        } catch (error) {
+          return Promise.reject(
+              new Error(`Failed to parse JSON: ${error.message}`),
+          );
+        }
+      },
+      status: response.statusCode,
+      headers: {
+        get: (headerName) => response.headers[headerName.toLowerCase()],
+      },
+      statusText: response.statusMessage,
+    };
+  },
+};
+
+export const initializeClient = (config) => {
+    return createClient({username: ZENDESK_USERNAME, subdomain: ZENDESK_SUBDOMAIN, ...config});
+};
+
+export const setupClient = (config = {}) =>
+    createClient({
+      username: ZENDESK_USERNAME,
+      subdomain: ZENDESK_SUBDOMAIN,
+      token: ZENDESK_TOKEN,
+      transportConfig: transportConfigUsingRequest,
+      ...config,
+    });
