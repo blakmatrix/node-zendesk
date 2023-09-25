2b54aab8c0081b8c29121908e494e9438bd252a2
Farrin Reid <blakmatrix@gmail.com>
2023-09-25 16:56:04 -0700

    docs: Add JSDoc comments and endpoints for Tags in node-zendesk
    This commit:
    - Adds detailed JSDoc comments to the Tags class and its methods in the node-zendesk project.
    - Introduces endpoints for listing, creating, retrieving, updating, and deleting tags.
    - Provides clear information on method usage, parameters, return values, and references to the Zendesk API documentation.
    
    These enhancements will aid developers in understanding and using the Tags API methods more effectively and ensure the library's alignment with the Zendesk API's capabilities.

--- src/client/core/tags.js
+++ src/client/core/tags.js
@@ -1,16 +1,89 @@
-// Tags.js: Client for the zendesk API.
-
 const {Client} = require('../client');
 
+/**
+ * Client for the Zendesk Tags API.
+ * @see {@link https://developer.zendesk.com/api-reference/sales-crm/resources/tags/}
+ */
 class Tags extends Client {
   constructor(options) {
     super(options);
     this.jsonAPINames = ['tags'];
   }
 
-  // Listing Tags
+  /**
+   * Retrieves all tags available to the user.
+   * @async
+   * @returns {Promise<Object[]>} A promise that resolves with the list of tags.
+   * @see {@link https://developer.zendesk.com/api-reference/sales-crm/resources/tags/#retrieve-all-tags}
+   * @example
+   * const client = createClient({...});
+   * const tags = await client.tags.list();
+   */
   async list() {
     return this.getAll(['tags']);
+  }
+
+  /**
+   * Creates a new tag.
+   * @async
+   * @param {Object} tagData - Data for the new tag.
+   * @param {string} tagData.name - Name of the tag.
+   * @param {string} tagData.resource_type - Type of resource the tag is attached to (lead, contact, deal).
+   * @returns {Promise<Object>} A promise that resolves with the created tag.
+   * @throws Will throw an error if the API call fails.
+   * @see {@link https://developer.zendesk.com/api-reference/sales-crm/resources/tags/#create-a-tag}
+   * @example
+   * const client = createClient({...});
+   * const newTag = await client.tags.create({ name: 'important', resource_type: 'contact' });
+   */
+  async create(tagData) {
+    return this.post(['tags'], tagData);
+  }
+
+  /**
+   * Retrieves a single tag by its ID.
+   * @async
+   * @param {number} id - Unique identifier of the tag.
+   * @returns {Promise<Object>} A promise that resolves with the retrieved tag.
+   * @throws Will throw an error if the API call fails.
+   * @see {@link https://developer.zendesk.com/api-reference/sales-crm/resources/tags/#retrieve-a-single-tag}
+   * @example
+   * const client = createClient({...});
+   * const tag = await client.tags.show(5);
+   */
+  async show(id) {
+    return this.get(['tags', id]);
+  }
+
+  /**
+   * Updates a tag's information.
+   * @async
+   * @param {number} id - Unique identifier of the tag.
+   * @param {Object} updatedData - Data to update.
+   * @returns {Promise<Object>} A promise that resolves with the updated tag.
+   * @throws Will throw an error if the API call fails.
+   * @see {@link https://developer.zendesk.com/api-reference/sales-crm/resources/tags/#update-a-tag}
+   * @example
+   * const client = createClient({...});
+   * const updatedTag = await client.tags.update(5, { name: 'super important' });
+   */
+  async update(id, updatedData) {
+    return this.put(['tags', id], updatedData);
+  }
+
+  /**
+   * Deletes a tag by its ID.
+   * @async
+   * @param {number} id - Unique identifier of the tag.
+   * @returns {Promise<void>} A promise that resolves when the tag has been deleted.
+   * @throws Will throw an error if the API call fails.
+   * @see {@link https://developer.zendesk.com/api-reference/sales-crm/resources/tags/#delete-a-tag}
+   * @example
+   * const client = createClient({...});
+   * await client.tags.delete(1);
+   */
+  async delete(id) {
+    return super.delete(['tags', id]);
   }
 }
 
