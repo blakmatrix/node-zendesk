13aa4c85356e9d479af0584e987afec13255d29c
Farrin Reid <blakmatrix@gmail.com>
2023-09-25 16:01:15 -0700

    Add detailed JSDoc comments for Sessions class
    This commit introduces comprehensive JSDoc comments to the Sessions
    class to provide clear documentation for developers. The comments
    include descriptions, async notation, parameters, return values,
    external links to the Zendesk API reference, usage examples, and
    potential errors thrown.
    
    Reference: Zendesk Sessions API documentation.

--- src/client/core/sessions.js
+++ src/client/core/sessions.js
@@ -1,37 +1,105 @@
-// Sessions.js: Client for the zendesk API.
+// File: sessions.js
 const {Client} = require('../client');
 
+/**
+ * Client class for interacting with Zendesk's Sessions API.
+ * @see {@link https://developer.zendesk.com/api-reference/ticketing/account-configuration/sessions/}
+ */
 class Sessions extends Client {
   constructor(options) {
     super(options);
     this.jsonAPINames = ['sessions'];
   }
 
-  // List Sessions
+  /**
+   * List all sessions.
+   * @async
+   * @returns {Promise<Object[]>} Array of sessions.
+   * @example
+   * const sessions = await client.sessions.get();
+   * @see {@link https://developer.zendesk.com/api-reference/ticketing/account-configuration/sessions/#list-sessions}
+   */
   async get() {
     return this.getAll(['sessions']);
   }
 
+  /**
+   * List sessions by user ID.
+   * @async
+   * @param {number} userId - The ID of the user.
+   * @returns {Promise<Object[]>} Array of sessions.
+   * @example
+   * const sessions = await client.sessions.getByUserId(12345);
+   * @see {@link https://developer.zendesk.com/api-reference/ticketing/account-configuration/sessions/#list-sessions}
+   */
   async getByUserId(userId) {
     return this.getAll(['users', userId, 'sessions']);
   }
 
+  /**
+   * Retrieve a specific session by user ID and session ID.
+   * @async
+   * @param {number} userId - The ID of the user.
+   * @param {number} sessionId - The ID of the session.
+   * @returns {Promise<Object>} Session details.
+   * @example
+   * const session = await client.sessions.getByUserIdBySessionId(12345, 67890);
+   * @see {@link https://developer.zendesk.com/api-reference/ticketing/account-configuration/sessions/#show-session}
+   */
   async getByUserIdBySessionId(userId, sessionId) {
     return this.get(['users', userId, 'sessions', sessionId]);
   }
 
+  /**
+   * Retrieve details of the currently authenticated session.
+   * @async
+   * @returns {Promise<Object>} Session details.
+   * @example
+   * const session = await client.sessions.getMyAuthenticatedSession();
+   * @see {@link https://developer.zendesk.com/api-reference/ticketing/account-configuration/sessions/#show-the-currently-authenticated-session}
+   */
   async getMyAuthenticatedSession() {
     return this.get(['users', 'me', 'session']);
   }
 
+  /**
+   * Delete a specific session by user ID and session ID.
+   * @async
+   * @param {number} userId - The ID of the user.
+   * @param {number} sessionId - The ID of the session.
+   * @returns {Promise<void>}
+   * @throws Will throw an error if the request fails.
+   * @example
+   * await client.sessions.deleteByUserIdBySessionId(12345, 67890);
+   * @see {@link https://developer.zendesk.com/api-reference/ticketing/account-configuration/sessions/#delete-session}
+   */
   async deleteByUserIdBySessionId(userId, sessionId) {
     return super.delete(['users', userId, 'sessions', sessionId]);
   }
 
+  /**
+   * Delete all sessions for a specific user by user ID.
+   * @async
+   * @param {number} userId - The ID of the user.
+   * @returns {Promise<void>}
+   * @throws Will throw an error if the request fails.
+   * @example
+   * await client.sessions.bulkDeleteByUserId(12345);
+   * @see {@link https://developer.zendesk.com/api-reference/ticketing/account-configuration/sessions/#bulk-delete-sessions}
+   */
   async bulkDeleteByUserId(userId) {
     return super.delete(['users', userId, 'sessions']);
   }
 
+  /**
+   * Logs out the current authenticated user.
+   * @async
+   * @returns {Promise<void>}
+   * @throws Will throw an error if the request fails.
+   * @example
+   * await client.sessions.logMeOut();
+   * @see {@link https://developer.zendesk.com/api-reference/ticketing/account-configuration/sessions/#delete-the-authenticated-session}
+   */
   async logMeOut() {
     return super.delete(['users', 'me', 'sessions']);
   }
