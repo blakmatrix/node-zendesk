b6760d1b0010502c7e33c06fb9fb3b2c10063f59
Farrin Reid <blakmatrix@gmail.com>
2023-09-25 17:05:03 -0700

    feat: Add detailed JSDoc for Targets class methods
    - Enhanced code documentation with JSDoc comments for the Targets class.
    - Documented methods with their descriptions, async status, params, return values, and relevant Zendesk API references.
    - Included example usage for each method to assist developers.
    - Noted possible exception cases.

--- src/client/core/targets.js
+++ src/client/core/targets.js
@@ -1,35 +1,86 @@
-// Targets.js: Client for the zendesk API.
 const {Client} = require('../client');
 
+/**
+ * Represents a client for the Zendesk Targets API.
+ * @see {@link https://developer.zendesk.com/api-reference/ticketing/targets/targets/}
+ */
 class Targets extends Client {
   constructor(options) {
     super(options);
     this.jsonAPINames = ['targets', 'target'];
   }
 
-  // Listing Targets
+  /**
+   * Lists all targets.
+   * @async
+   * @returns {Promise<Object>} A promise that resolves to the list of targets.
+   * @see {@link https://developer.zendesk.com/api-reference/ticketing/targets/targets/#list-targets}
+   * @example
+   * const targets = await client.targets.list();
+   */
   async list() {
     return this.getAll(['targets']);
   }
 
-  // Viewing Targets
-  async show(triggerID) {
-    return this.get(['targets', triggerID]);
+  /**
+   * Retrieves details of a specific target.
+   * @async
+   * @param {number} targetId - The ID of the target to retrieve.
+   * @returns {Promise<Object>} A promise that resolves to the target details.
+   * @see {@link https://developer.zendesk.com/api-reference/ticketing/targets/targets/#show-target}
+   * @example
+   * const target = await client.targets.show(12345);
+   */
+  async show(targetId) {
+    return this.get(['targets', targetId]);
   }
 
-  // Creating Targets
+  /**
+   * Creates a new target.
+   * @async
+   * @param {Object} target - The target data.
+   * @returns {Promise<Object>} A promise that resolves to the created target's details.
+   * @see {@link https://developer.zendesk.com/api-reference/ticketing/targets/targets/#create-target}
+   * @example
+   * const target = {
+   *   type: 'email_target',
+   *   title: 'Test Email Target',
+   *   email: 'hello@example.com',
+   *   subject: 'Test Target'
+   * };
+   * const response = await client.targets.create(target);
+   */
   async create(target) {
     return this.post(['targets'], target);
   }
 
-  // Updating Targets
-  async update(targetID, target) {
-    return this.put(['targets', targetID], target);
+  /**
+   * Updates a specific target.
+   * @async
+   * @param {number} targetId - The ID of the target to update.
+   * @param {Object} target - The updated target data.
+   * @returns {Promise<Object>} A promise that resolves to the updated target's details.
+   * @see {@link https://developer.zendesk.com/api-reference/ticketing/targets/targets/#update-target}
+   * @example
+   * const updatedData = { email: 'updated@example.com' };
+   * const response = await client.targets.update(12345, updatedData);
+   */
+  async update(targetId, target) {
+    return this.put(['targets', targetId], target);
   }
 
-  // Deleting Targets
-  async delete(targetID) {
-    return super.delete(['targets', targetID]);
+  /**
+   * Deletes a specific target.
+   * @async
+   * @param {number} targetId - The ID of the target to delete.
+   * @returns {Promise<void>} A promise that resolves once the target has been deleted.
+   * @throws {Error} Throws an error if deletion fails.
+   * @see {@link https://developer.zendesk.com/api-reference/ticketing/targets/targets/#delete-target}
+   * @example
+   * await client.targets.delete(12345);
+   */
+  async delete(targetId) {
+    return super.delete(['targets', targetId]);
   }
 }
 
