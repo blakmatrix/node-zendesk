5f5b0545eefdd15efc4d60e244d56d7ea3b020e4
Farrin Reid <blakmatrix@gmail.com>
2023-09-25 17:45:30 -0700

    feat: Add TicketAudits methods with JSDoc documentation
    - Added detailed JSDoc documentation to `TicketAudits` class.
    - Implemented the following methods:
      - `listAll()`: Fetches all ticket audits.
      - `list(ticketID)`: Lists all audits for a specific ticket.
      - `count(ticketID)`: Retrieves an approximate count of audits for a ticket.
      - `show(ticketID, auditID)`: Fetches details of a specific ticket audit.
      - `makePrivate(ticketID, auditID)`: Changes a comment from public to private for an audit.
    
    Each method includes:
    - Description of its functionality.
    - Appropriate JSDoc tags (`@async`, `@param`, `@returns`, `@throws`, `@see`, and `@example`).
    - Corresponding Zendesk API references linked via `@see` tags.

--- src/client/core/ticketaudits.js
+++ src/client/core/ticketaudits.js
@@ -1,6 +1,9 @@
-// TicketAudits.js: Client for the zendesk API.
 const {Client} = require('../client');
 
+/**
+ * Client for the Zendesk Ticket Audits API.
+ * @see {@link https://developer.zendesk.com/api-reference/ticketing/tickets/ticket_audits/}
+ */
 class TicketAudits extends Client {
   constructor(options) {
     super(options);
@@ -11,9 +14,80 @@ class TicketAudits extends Client {
     ];
   }
 
-  // Listing TicketAudits
+  /**
+   * List all ticket audits. Note: Archived tickets are not included.
+   * @async
+   * @returns {Promise<Array<Object>>} Returns an array of ticket audit objects.
+   * @throws {Error} Throws an error if request fails.
+   * @see {@link https://developer.zendesk.com/api-reference/ticketing/tickets/ticket_audits/#list-all-ticket-audits}
+   * @example
+   * const client = createClient({...});
+   * const allAudits = await client.ticketaudits.listAll();
+   */
+  async listAll() {
+    return this.get('ticket_audits');
+  }
+
+  /**
+   * List all audits for a specified ticket.
+   * @async
+   * @param {number} ticketID - The ID of the ticket.
+   * @returns {Promise<Array<Object>>} Returns an array of ticket audit objects.
+   * @throws {Error} Throws an error if request fails.
+   * @see {@link https://developer.zendesk.com/api-reference/ticketing/tickets/ticket_audits/#list-audits-for-a-ticket}
+   * @example
+   * const client = createClient({...});
+   * const audits = await client.ticketaudits.list(12345);
+   */
   async list(ticketID) {
     return this.getAll(['tickets', ticketID, 'audits']);
+  }
+
+  /**
+   * Get an approximate count of audits for a specified ticket.
+   * @async
+   * @param {number} ticketID - The ID of the ticket.
+   * @returns {Promise<number>} Returns an approximate count of audits.
+   * @throws {Error} Throws an error if request fails.
+   * @see {@link https://developer.zendesk.com/api-reference/ticketing/tickets/ticket_audits/#count-audits-for-a-ticket}
+   * @example
+   * const client = createClient({...});
+   * const auditCount = await client.ticketaudits.count(12345);
+   */
+  async count(ticketID) {
+    return this.get(['tickets', ticketID, 'audits', 'count']);
+  }
+
+  /**
+   * Show details of a specific ticket audit.
+   * @async
+   * @param {number} ticketID - The ID of the ticket.
+   * @param {number} auditID - The ID of the ticket audit.
+   * @returns {Promise<Object>} Returns details of the ticket audit.
+   * @throws {Error} Throws an error if request fails.
+   * @see {@link https://developer.zendesk.com/api-reference/ticketing/tickets/ticket_audits/#show-audit}
+   * @example
+   * const client = createClient({...});
+   * const auditDetails = await client.ticketaudits.show(12345, 67890);
+   */
+  async show(ticketID, auditID) {
+    return this.get(['tickets', ticketID, 'audits', auditID]);
+  }
+
+  /**
+   * Change a comment from public to private for a specific ticket audit.
+   * @async
+   * @param {number} ticketID - The ID of the ticket.
+   * @param {number} auditID - The ID of the ticket audit.
+   * @returns {Promise<Object>} Returns the updated ticket audit details.
+   * @throws {Error} Throws an error if request fails.
+   * @see {@link https://developer.zendesk.com/api-reference/ticketing/tickets/ticket_audits/#change-a-comment-from-public-to-private}
+   * @example
+   * const client = createClient({...});
+   * await client.ticketaudits.makePrivate(12345, 67890);
+   */
+  async makePrivate(ticketID, auditID) {
+    return this.put(['tickets', ticketID, 'audits', auditID, 'make_private']);
   }
 }
 
