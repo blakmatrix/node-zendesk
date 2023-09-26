3796ae9a6109ef00619a8cc513b51c7f86d69e9e
Farrin Reid <blakmatrix@gmail.com>
2023-09-25 17:00:08 -0700

    Add JSDoc comments for TargetFailures class
    - Added detailed JSDoc comments for the TargetFailures class.
    - Documented methods `list` and `show` with descriptions, parameters, return values, examples, and potential exceptions.
    - Provided links to the Zendesk API for additional context.

--- src/client/core/targetfailures.js
+++ src/client/core/targetfailures.js
@@ -1,20 +1,43 @@
 // Targetfailures.js: Client for the zendesk API.
 const {Client} = require('../client');
 
+/**
+ * A class for interacting with the Zendesk Target Failures API.
+ * @see {@link https://developer.zendesk.com/api-reference/ticketing/targets/target_failures/}
+ */
 class TargetFailures extends Client {
   constructor(options) {
     super(options);
     this.jsonAPINames = ['targetfailures', 'targetsfailures'];
   }
 
-  // Listing Target Failures
+  /**
+   * Lists the 25 most recent target failures, per target.
+   * @async
+   * @return {Array} An array of target failures.
+   * @example
+   * const client = createClient({...});
+   * const failures = await client.targetfailures.list();
+   * @see {@link https://developer.zendesk.com/api-reference/ticketing/targets/target_failures/#list-target-failures}
+   * @throws {Error} Throws an error if the request fails.
+   */
   async list() {
-    this.getAll(['target_failures']);
+    return this.getAll(['target_failures']);
   }
 
-  // Viewing Target Failure
+  /**
+   * Retrieves the details of a specific target failure by its ID.
+   * @async
+   * @param {number} targetFailureID - The ID of the target failure.
+   * @return {Object} The target failure details.
+   * @example
+   * const client = createClient({...});
+   * const failureDetails = await client.targetfailures.show(6001326);
+   * @see {@link https://developer.zendesk.com/api-reference/ticketing/targets/target_failures/#show-target-failure}
+   * @throws {Error} Throws an error if the request fails or if the target failure ID is not found.
+   */
   async show(targetFailureID) {
-    this.get(['target_failures', targetFailureID]);
+    return this.get(['target_failures', targetFailureID]);
   }
 }
 
