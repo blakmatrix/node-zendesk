3a23f2d6b729fb781ffbb9f5a2c1bac427591d47
Farrin Reid <blakmatrix@gmail.com>
2023-10-08 10:13:20 -0700

    remove `request` from test as `cross-fetch` works

--- test/setup.js
+++ test/setup.js
@@ -2,7 +2,6 @@
 
 import process from 'node:process';
 import dotenv from 'dotenv';
-import request from 'request';
 import {createClient} from '../src/index.js';
 
 dotenv.config({
@@ -26,47 +25,6 @@ export const generateMultipleOrganizations = (n) => {
   return organizations;
 };
 
-export const transportConfigUsingRequest = {
-  transportFn(uri, options) {
-    // Convert the options to be compatible with the request library
-    const requestOptions = {
-      ...options,
-      uri,
-      headers: options.headers,
-    };
-
-    return new Promise((resolve, reject) => {
-      request(requestOptions, (error, response, body) => {
-        if (error) {
-          reject(error);
-        } else {
-          // Include the body in the response object for the adapter to handle
-          response.body = body;
-          resolve(response);
-        }
-      });
-    });
-  },
-
-  responseAdapter(response) {
-    return {
-      json() {
-        try {
-          return Promise.resolve(JSON.parse(response.body));
-        } catch (error) {
-          return Promise.reject(
-            new Error(`Failed to parse JSON: ${error.message}`),
-          );
-        }
-      },
-      status: response.statusCode,
-      headers: {
-        get: (headerName) => response.headers[headerName.toLowerCase()],
-      },
-      statusText: response.statusMessage,
-    };
-  },
-};
 
 export const initializeClient = (config) => {
   return createClient({
@@ -81,6 +39,5 @@ export const setupClient = (config = {}) =>
     username: ZENDESK_USERNAME,
     subdomain: ZENDESK_SUBDOMAIN,
     token: ZENDESK_TOKEN,
-    transportConfig: transportConfigUsingRequest,
     ...config,
   });
