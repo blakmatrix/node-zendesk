d204b4a103798a2ee90cda69347ae20f6a80a9ad
Mike Hoyle <149415898+mike-hoyle@users.noreply.github.com>
2024-03-19 22:59:58 +0000

    Fix Organization membership creation

--- src/clients/core/organizationmemberships.js
+++ src/clients/core/organizationmemberships.js
@@ -87,29 +87,30 @@ class OrganizationMemberships extends Client {
 
   /**
    * Create a new organization membership.
-   * @param {object} organizationMembership - The organization membership data.
-   * @returns {Promise<object>} A promise resolving to the created organization membership.
+   * @param {OrganizationMembership} organizationMembership - The organization membership data.
+   * @returns {Promise<OrganizationMembership>} A promise resolving to the created organization membership.
    * @async
    * @see {@link https://developer.zendesk.com/api-reference/ticketing/organizations/organization_memberships/#create-membership}
    * @example
    * const newMembership = await client.organizationmemberships.create({ user_id: 123, organization_id: 456 });
    */
   async create(organizationMembership) {
-    return this.put(['organization_memberships'], organizationMembership);
+    return this.post(['organization_memberships'], organizationMembership);
   }
 
   /**
    * Create a new organization membership for a specific user.
    * @param {number} userID - The user ID.
    * @param {object} organizationMembership - The organization membership data.
-   * @returns {Promise<object>} A promise resolving to the created organization membership.
+   * @param {number} organizationMembership.organization_id - The organization id of the membership.
+   * @returns {Promise<OrganizationMembership>} A promise resolving to the created organization membership.
    * @async
    * @see {@link https://developer.zendesk.com/api-reference/ticketing/organizations/organization_memberships/#create-membership}
    * @example
    * const newMembership = await client.organizationmemberships.createByUser(123, { organization_id: 456 });
    */
   async createByUser(userID, organizationMembership) {
-    return this.put(
+    return this.post(
       ['users', userID, 'organization_memberships'],
       organizationMembership,
     );
